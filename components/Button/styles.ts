77bf6288df0585fc98c5d22eefb0c3bf033fa972
Heitor Kuba <s2it_hkuba@uolinc.com>
2019-08-07 20:05:20 -0300

    Button component

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -1,17 +1,47 @@
 import css from 'styled-jsx/css';
-import { COLOR_PRIMARY, COLOR_SUCCESS, COLOR_ALERT, COLOR_ERROR } from '../../constants/colors';
+import { COLOR_PRIMARY } from '../../constants/colors';
+import { SIZE_CTA } from '../../constants/typography';
+
+const primaryStyles = `
+    color: white;
+    background-color: ${COLOR_PRIMARY};
+`;
+
+const primaryAltStyles = `
+    color: ${COLOR_PRIMARY};
+    background-color: transparent;
+    border: 2px solid ${COLOR_PRIMARY};
+`;
 
 export const styles = css`
     .button {
         height: 40px;
         padding: 0 32px;
+        font-size: ${SIZE_CTA};
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 20px;
+        border: none;
+        cursor: pointer;
+    }
+
+    .button:focus {
+        outline: none;
     }
 
     .button--primary {
-        background-color: ${COLOR_PRIMARY};
+        ${primaryStyles}
+    }
+
+    .button--primary:active {
+        ${primaryAltStyles}
     }
 
     .button--primary-alt {
-        background-color: ${COLOR_SUCCESS};
+        ${primaryAltStyles}
+    }
+
+    .button--primary-alt:active {
+        ${primaryStyles}
     }
 `;
